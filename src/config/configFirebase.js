6f7bc8ca3964b4e2eb9b53a2ac4f6c391ec055f9
vicha3663 <f.tramites@gmail.com>
2021-07-03 00:54:00 -0400

    tres

---
+++ src/config/configFirebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAt8yMwN_pEGsVT6ywuAMF016o3RxwiFlg",
+    authDomain: "ottokrauss.firebaseapp.com",
+    projectId: "ottokrauss",
+    storageBucket: "ottokrauss.appspot.com",
+    messagingSenderId: "623178272002",
+    appId: "1:623178272002:web:e5d21b1b95af2847b8461b"
+  };
